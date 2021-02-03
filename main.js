48007709c903687081d015e6e304f36608594e28
kyriehavegoodday <healthymbear@gmail.com>
2021-02-03 21:11:05 +0800

    作品集完整版

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ $('#banner').slick({
     autoplay:true,
     autoplaySpeed:1500
 });
+$("#works").slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive:[
+        {
+            breakpoint:1024,
+            settings: {
+                slidesToShow:3,
+                slidesToScroll:1,
+            }
+        },
+        {
+            breakpoint:767,
+            settings: {
+                slidesToShow:1,
+                slidesToScroll:1,
+            }
+        }
+    ]
+});
+
+$('menu').click({
+    
+});
